170ffc43f636b412480419489042eb59c3104776
Devon Terry <114770792+crumbycupid@users.noreply.github.com>
2022-11-24 02:51:28 -0500

    strings

--- javascript/code-challenges/challenges-12.test.js
+++ javascript/code-challenges/challenges-12.test.js
@@ -65,7 +65,7 @@ Note: if you ever need to validate an email using a regex in practice, the Inter
 ------------------------------------------------------------------------------------------------ */
 
 const validateEmail = (email) => {
-  // Solution code here...
+ /@[a-zA-Z0-9]+(.com|.net|.org)$
 };
 
 /* ------------------------------------------------------------------------------------------------
