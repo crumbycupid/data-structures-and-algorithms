f778668f7e4a39d54e882d5170d185fcb134309c
Devon Terry <114770792+crumbycupid@users.noreply.github.com>
2023-01-17 20:31:58 -0500

    initial commit

--- java/datastructures/lib/src/main/java/datastructures/linkedlist/LinkedList.java
+++ java/datastructures/lib/src/main/java/datastructures/linkedlist/LinkedList.java
@@ -28,10 +28,48 @@ public class LinkedList
     return false;
   }
 
+  public void Append(int value){
+    Node newNode = new Node();
+    newNode.value = value;
+    Node curr = head;
+
+    if (head = null){
+      head = newNode;
+    }else{
+      curr = head;
+      while (curr.next !=null) {
+        curr.next = newNode;
+      }
+  }
+
+    public void insertBefore(int value int newNodeValue){
+      Node newNode = new Node();
+      Node curr = head;
+      newNode.value = newNodeValue;
+
+      if(head.value == value){
+        newNode.next = head;
+        head = newNode;
+      } while(curr.next !null && curr.next.value !=value){
+        curr = curr.next;
+      } if(curr.next != null){
+        Node insert = curr.next;
+        curr.next = newNode;
+        newNode.next = insert;
+        return;
+      }
+    }
+
+    public void insertAfter(int valueA, int valueB){
+      Node newNode =
+    }
+
   @Override
   public String toString()
   {
     return "hello";  // TODO: implement me
   }
 }
+
+
 
